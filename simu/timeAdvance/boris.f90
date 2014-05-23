64745c4bb00186a910e4e0d6756cb682c3618d07
Candyfeline <candyfeline@CFMac.local>
2014-05-23 14:45:56 +0800

    Corrected the boris algorithm which was errorly computed due to mistakes in paperwork.

--- simu/timeAdvance/boris.f90
+++ simu/timeAdvance/boris.f90
@@ -6,6 +6,8 @@
 ! Except for the licence, it is kind to contact me
 ! if the codes are involved in your original research.
 
+module boris
+contains
 elemental function borisOneStep(vOld, B, E, q, m, c, deltaT) result (vNew)
   use vec3d
   implicit none
@@ -14,14 +16,16 @@ elemental function borisOneStep(vOld, B, E, q, m, c, deltaT) result (vNew)
   double precision, intent (in) :: q, m, c, deltaT
   type(vec3) :: vNew
 
-  type(vec3) :: vMinus, vPlus, bigC
+  type(vec3) :: vMinus, vPlus
 
   vMinus = vOld + (q * deltaT / 2 / m) * E
-  bigC = vMinus + (q / 2 / m / c) * (vMinus .x. B) 
-  vPlus = (deltaT * bigC + &
-           (q * deltaT**2 / (2 * m * c)) * (bigC .x. B) + &
-           (q * deltaT**2 / (2 * m * c) * (bigC .dot. B)) * B) &
+  vPlus = (vMinus + &
+           (q * deltaT / (m * c)) * (vMinus .x. B) - &
+           (q**2 * deltaT**2 / (4 * m**2 * c**2) * (B .dot. B) * vMinus) +&
+           (q**2 * deltaT**2 / (2 * m**2 * c**2) * (vMinus .dot. B)) * B &
+          ) &
           / &
           (1 + q**2 * (B .dot. B) * deltaT**2 / (4 * m**2 * c**2))
   vNew = vPlus + (q * deltaT / 2 / m) * E
 end function borisOneStep
+end module boris
