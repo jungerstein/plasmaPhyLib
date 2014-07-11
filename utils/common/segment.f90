d5cd9a8c8b8990cf8746215d28fb6634dbe347d1
jungerstein <BeatlesFan1987@gmail.com>
2014-07-11 18:40:05 +0800

    Add service program to decide a subscript (unsafe).

---
+++ utils/common/segment.f90
@@ -0,0 +1,16 @@
+! Segment.f90
+! Operations about segments. 
+
+module segment
+contains
+elemental function whichSegment(x, xLower,xUpper, iLower,iUpper) result (i)
+  implicit none
+  double presicion, intent (in) :: x - xLower, xUpper
+  integer, intent (in) :: iLower, iUpper
+
+  integer :: i
+
+  i = iLower + floor(0.5 + (x-xLower) / (xUpper-xLower) * (iUpper-iLower))
+end function whichSegment
+
+end module segment
