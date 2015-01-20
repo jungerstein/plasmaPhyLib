08395126acab384ca898eebfd2808ff17bebbce9
jungerstein <BeatlesFan1987@gmail.com>
2015-01-20 20:56:55 +0800

    Add curl for 2.5D spherical mesh, only parsing A_theta.

--- calculus/sphere.f90
+++ calculus/sphere.f90
@@ -120,6 +120,45 @@ contains
     end do
   end subroutine doGrad
 
+  ! Special as to only A_theta is parsed. 
+  subroutine doCurl(res, Atheta, meshEquator)
+    implicit none
+                                           !  r, phi
+    double precision, intent (in), dimension (:, :) :: Atheta
+    type (meshSphereEquator), intent (in) :: meshEquator
+    type (vecSphere), intent (out), &
+      dimension (size(vals, 1) - 1, size(vals, 2) - 1) :: res
+
+    integer :: iR, iPhi, nR, nPhi
+    double precision :: r, dr, dPhi
+
+    nR = size(vals, 1)
+    nPhi = size(vals, 2)
+
+    checkGrid(meshEquator%nR, nR, 'r mesh')
+    checkGrid(meshEquator%nPhi, nPhi, 'phi mesh')
+
+    dr = calcDr(meshEquator)
+    dPhi = calcDphi(meshEquator)
+
+    do iPhi = 1, nPhi - 1
+      do iR = 1, nR - 1
+        r = calcR(iR, dr, meshEquator) 
+        ! curl A_r = - 1 / r * (partial Atheta / partial phi) 
+        res(iR, iPhi) % r = - 1 / r * &
+          (Atheta(iR, iPhi + 1) - Atheta(iR, iPhi)) / dPhi
+        ! grad_theta u = 0
+        res(iR, iPhi) % theta = 0
+        ! grad_phi u = (1/r) (partial (r Atheta) / partial r)
+        res(iR, iPhi) % phi = 1 / r * &
+          ( &
+              (r + dr / 2) * Atheta(iR + 1, iPhi) &
+            - (r - dr / 2) * Atheta(iR,     iPhi) &
+          ) / dr
+      end do
+    end do
+  end subroutine doCurl
+
   subroutine checkGrid(a, b, namen)
     implicit none
     integer, intent (in) :: a, b
