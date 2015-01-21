5cafc7904f0dbc3b4a69b239c026e4d67565ea44
jungerstein <BeatlesFan1987@gmail.com>
2015-01-21 09:46:15 +0800

    Undo the hypercorrection in laplacian w equator sph mesh.

--- calculus/sphere.f90
+++ calculus/sphere.f90
@@ -249,7 +249,7 @@ contains
           ) / dr &
           ! + 1/(r^2) f,PhiPhi 
           + 1 / (r ** 2) * &
-            (vals(iR + 1, iPhi + 1) + vals(iR + 1, iPhi - 1) - 2 * vals(iR + 1, iPhi)) &
+            (vals(iR, iPhi + 1) + vals(iR, iPhi - 1) - 2 * vals(iR, iPhi)) &
             / (dPhi ** 2)
       end do
     end do
